fdb87b60ce9ea313ce291b4485140ab6494dcdf5
susmote <admin@susmote.com>
2018-10-09 17:15:11 +0800

    change url

--- article_readnum/main.py
+++ article_readnum/main.py
@@ -11,8 +11,8 @@ import time
 
 
 if __name__ == '__main__':
-    session = requests.session();
+    session = requests.session()
     while(True):
-        atc_content = session.get("https://media.weibo.cn/article?id=2309404292838102854976").text
-        print(atc_content[atc_content.find("read_count"):atc_content.find("read_count")+22])
+        atc_content = session.get("https://media.weibo.cn/article?id=2309404293255578702310").text
+        print(atc_content[atc_content.find("read_count"):atc_content.find("read_count")+20])
         time.sleep(1)
