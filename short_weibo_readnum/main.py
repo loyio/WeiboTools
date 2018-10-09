01a972f00f5e945e2d7081a854310c3028499369
susmote <susmote@vip.qq.com>
2018-10-09 12:55:35 +0800

    update short weibo readnum

--- short_weibo_readnum/main.py
+++ short_weibo_readnum/main.py
@@ -13,6 +13,6 @@ import time
 if __name__ == '__main__':
     session = requests.session();
     while(True):
-        atc_content = session.get("https://media.weibo.cn/article?id=2309404292838102854976").text
+        atc_content = session.get("https://m.weibo.cn/status/4292838109889566?").text
         print(atc_content[atc_content.find("read_count"):atc_content.find("read_count")+22])
         time.sleep(1)
