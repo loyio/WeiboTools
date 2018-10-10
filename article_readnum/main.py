00704f435dd8c09ea7f35daebf12349841027efd
susmote <admin@susmote.com>
2018-10-10 21:06:03 +0800

    update new  link

--- article_readnum/main.py
+++ article_readnum/main.py
@@ -13,6 +13,6 @@ import time
 if __name__ == '__main__':
     session = requests.session()
     while(True):
-        atc_content = session.get("https://media.weibo.cn/article?id=2309404293590330265956").text
+        atc_content = session.get("https://media.weibo.cn/article?id=2309404292561916325492").text
         print(atc_content[atc_content.find("read_count"):atc_content.find("read_count")+23])
         time.sleep(1)
