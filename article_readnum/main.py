ea94f900f478e4350f247d31556299dac49790f5
susmote <admin@susmote.com>
2018-10-11 20:14:01 +0800

    update new function

--- article_readnum/main.py
+++ article_readnum/main.py
@@ -16,13 +16,14 @@ if __name__ == '__main__':
     if(len(sys.argv) < 2):
         print("请在后面加上头条文章链接")
     else:
-        session = requests.session()
-        atc_content = session.get(sys.argv[1]).text
         while(True):
+            session = requests.session()
+            atc_content = session.get(sys.argv[1]).text
             read_num = atc_content[atc_content.find("read_count"):atc_content.find("read_count")+23]
             if(read_num == read_num_cmp):
                 print("已被官方限制，暂停阅读量更新")
                 quit(0)
             else:
                 read_num_cmp = read_num
+                print(read_num_cmp)
             time.sleep(1)
