8cef7c506311d6827a37575bd7dc0073e3f429b4
Loyio <i@loyio.me>
2021-03-12 16:51:32 +0800

    Update login_account.py

--- Weibo_MiniApp_Api/login_account.py
+++ Weibo_MiniApp_Api/login_account.py
@@ -10,20 +10,5 @@ user_account = [
     {
         "gsid": "_2A_p6SXYtCoX0ky4rkw3dSFsN3czlPcwoRPDnzmFeEtLG8oFeQ6wiWFSEZASDtSCPQ86sCjF-299smA5TysrImhF-",
         "s": "95ed5c13"
-    },
-    #盛夏仙越
-    {
-        "gsid": "_2A_p6SUQtCoX0ky4qkg3cQ1QN3MHlPcwoROLnzmFeEtLG8oFeQ5UiWFSEZN4d2FW6VoHDVdsLmCYwKva4rmWePUo1",
-        "s": "8024905d"
-    },
-    #susmote110
-    {
-        "gsid": "_2A_p6STPZCoV0kysrkAjbTlIP0sTlPcwoSzwTzmFeEtLG44FeQ6gtWFSEbZ9mHDMWXzfevtmrtmGKluuA2yBdyjGt",
-        "s": "9a818883"
-    },
-    #杨超越小黄包数据站
-    {
-        "gsid": "_2A_p6SM7hCoX0kysrkAnaQ1EM3M3lPcwoS2crzmFeEtLG8oFZQ4glWFSEbZ2a_NHu-mIxrh4xwx3mAXbNwW2v0Q7d",
-        "s": "6ca339d5"
     }
 ]
