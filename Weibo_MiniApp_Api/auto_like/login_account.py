659232b4525bcbedf350da1127d382ff6c6e9e71
Loyio <i@loyio.me>
2021-03-12 16:53:06 +0800

    Update login_account.py

--- Weibo_MiniApp_Api/auto_like/login_account.py
+++ Weibo_MiniApp_Api/auto_like/login_account.py
@@ -10,15 +10,5 @@ user_account = [
     {
         "gsid": "_2A_p6SRwbCoX0ky4rkw3dSFsN3czlPcwoSwrRzmFeEtLG44FeQ8wYWFSEZASDtbw6xJh-HxDL4RQ3qCLDU3ao8pRA",
         "s": "95ed5c13"
-    },
-    #盛夏仙越
-    {
-        "gsid": "_2A_p6SUQtCoX0ky4qkg3cQ1QN3MHlPcwoROLnzmFeEtLG8oFeQ5UiWFSEZN4d2FW6VoHDVdsLmCYwKva4rmWePUo1",
-        "s": "8024905d"
-    },
-    #susmote110
-    {
-        "gsid": "_2A_p6STPZCoV0kysrkAjbTlIP0sTlPcwoSzwTzmFeEtLG44FeQ6gtWFSEbZ9mHDMWXzfevtmrtmGKluuA2yBdyjGt",
-        "s": "9a818883"
     }
 ]
